8693d997575e512e81b24b6b0444dcbdeef73570
oloyede jamiu <oloyedejamiu@gmail.com>
2017-08-19 16:21:54 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+  $(document).ready(function() {
+  
+    //$(".well").addClass("animated shake"); <span class="pull-right"><i class="fa fa-check fa-2x" aria-hidden="true"></i></span>
+
+   $("#general-signup").appendTo( ".done" );
+  });
+
+  $(document).ready(function() {
+  //carousel options
+  $('#quote-carousel').carousel({
+    pause: true, interval: 10000,
+  });
+});
+
+
+
